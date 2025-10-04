d13dbee2739242095d65740c6acc15d4ca49658f
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-10-03 18:42:49 -0700

    HW

---
+++ List_Comprehension.py
@@ -0,0 +1,3 @@
+test_list = ['Codingal',3, 'Coding',2, 'python',6, 'Typing', 'CPU',8, 'Codingal']
+newlist1 = [x for x in test_list if x == str('Codingal')]
+print("Test Comphrehension:\n", newlist1)
